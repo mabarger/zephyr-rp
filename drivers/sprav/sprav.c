5a08e3a1a8b6d47d2646d9a5706ad9445162bd56
mabarger <65674651+mabarger@users.noreply.github.com>
2023-05-21 15:32:21 +0200

    Improve code organization

--- drivers/sprav/sprav.c
+++ drivers/sprav/sprav.c
@@ -10,14 +10,22 @@
 #include <zephyr/drivers/sprav.h>
 #include <zephyr/logging/log.h>
 
+#include <tinycrypt/sha256.h>
+#include <tinycrypt/constants.h>
+
 /* Start and End locations for the prac section */
 extern uint8_t *prac_begin;
 extern uint8_t *prac_end;
 
-int __attribute__((section("prac"))) sprav_attest_region_protected(uintptr_t addr, size_t size, uint64_t nonce,
-								   uint8_t *signature)
+int sprav_attest_region_protected(uintptr_t addr, size_t size, uint64_t nonce,
+				  uint8_t *signature)
 {
 	/* Compute Hash over requested region */
+	struct tc_sha256_state_struct ctx;
+
+	tc_sha256_init(&ctx);
+	tc_sha256_update(&ctx, (uint8_t *) addr, size);
+	tc_sha256_final(signature, &ctx);
 
 	return 0;
 }
